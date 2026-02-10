9481741e7fe9ff2340602177438e4a1de359af3c
Tony Barelli <barellitony13@gmail.com>
2026-02-10 22:53:33 +0100

    Ajout footer

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -14,6 +14,7 @@ import SupportIcon from './icons/IconSupport.vue'*/
     </template>
     <template #heading>Présentation</template>
     <p>Je me présente, je suis un développeur passionné par la création de sites web et d'applications.</p>
+    <p>Je suis actuellement en formation de developpeur web et mobile chez Dawan.</p>
   </WelcomeItem>
   <!--<SupportIcon /> <br>
   <CommunityIcon /> <br>
